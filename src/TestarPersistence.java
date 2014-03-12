4516694c467467c1ca0130399c80bac5ae4acd10
alessandrostein <ale182juve@msn.com>
2014-03-12 00:14:14 -0300

    Organização do código e removido importações desnecessarias

--- src/TestarPersistence.java
+++ src/TestarPersistence.java
@@ -1,3 +1,4 @@
+
 import hibernate.persistence.dao.RoleDAO;
 import java.util.ArrayList;
 import hibernate.persistence.entities.User;
@@ -7,73 +8,75 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestarPersistence {
- 
+
     public static void main(String[] args) {
         try {
-            createUsers();
+            createUsersRoles();
             showAllUser();
         } catch (Exception ex) {
             System.out.println("A tentativa de criar usuários falhou!/n... " + ex.getMessage());
         }
-        
+
     }
-    
-    private static void createUsers() throws Exception {
-        System.out.println("Criando usuários...");
-        
+
+    private static void createUsersRoles() throws Exception {
+        System.out.println("Criando regras...");
+
         RoleDAO roledao = new RoleDAO();
+        
         Role role1 = (Role) roledao.getNewInstance();
+        role1.setName("Role 1");
+        
         Role role2 = (Role) roledao.getNewInstance();
+        role2.setName("Role 2");
         
-        role1.setName("Regra 1");
         roledao.save(role1);
-        
-        role2.setName("Regra 2");
         roledao.save(role2);
         
+        System.out.println("Criando usuários...");
+
         UserDAO dao = new UserDAO();
+        
         User admin = (User) dao.getNewInstance();
         admin.setName("Administrador");
 
         User guest = (User) dao.getNewInstance();
         guest.setName("Convidado");
-        
+
         User user = (User) dao.getNewInstance();
         user.setName("Ayrton Senna");
+
+        System.out.println("Relacionando usuários e regras...");
 
         Set roles = new HashSet();
         roles.add(role1);
         roles.add(role2);
-        
+
         user.setRole(roles);
         admin.setRole(roles);
-        //guest.setRole(roles);
-        
-         dao.save(user);
+        guest.setRole(roles);
+
+        dao.save(user);
         dao.save(admin);
         dao.save(guest);
-       
 
         System.out.print(" OK!");
 
-    }    
-    
-    private static void showAllUser() throws Exception { 
+    }
+
+    private static void showAllUser() throws Exception {
         System.out.println("Listando usuários...");
         UserDAO dao = new UserDAO();
         ArrayList users = (ArrayList) dao.findAll();
         User o;
-        
-        for (int i=0; i<users.size(); i++) {
+
+        for (int i = 0; i < users.size(); i++) {
             o = (User) users.get(i);
             System.out.println(o);
         }
-        
-        
-        User admin = (User) dao.getNewInstance();    
-    
-    
+
+        User admin = (User) dao.getNewInstance();
+
     }
-    
-    
+
 }
