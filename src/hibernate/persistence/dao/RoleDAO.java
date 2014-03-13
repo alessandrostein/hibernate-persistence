af1ede48f326b17d6be53ea05f031bf3ef975042
alessandrostein <ale182juve@msn.com>
2014-03-13 00:28:05 -0300

    Implementação findUser

--- src/hibernate/persistence/dao/RoleDAO.java
+++ src/hibernate/persistence/dao/RoleDAO.java
@@ -6,13 +6,16 @@ package hibernate.persistence.dao;
 
 import hibernate.persistence.entities.Role;
 import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 
 /**
  *
  * @author alessandro.stein
  */
-public class RoleDAO extends AbstractDAO implements IRoleDAO{
-    
+public class RoleDAO extends AbstractDAO implements IRoleDAO {
+
     @Override
     protected String getNamedQueryToFindAll() {
         return "role.find.all";
@@ -42,16 +45,31 @@ public class RoleDAO extends AbstractDAO implements IRoleDAO{
     protected String getNamedQueryToFindByRange() {
         return "role.find.range";
     }
-    
+
     public Object getNewInstance() {
         return new Role();
-    } 
-
-    
-    // Implementação da busca do usuario.
-    @Override
-    public ArrayList findUser(Role o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    protected String getNameQueryToFindUser() {
+        return "role.find.user";
+    }
+
+    // Implementação da busca do usuario.
+    @Override
+    public List findUser(Role o) throws Exception {
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.getNamedQuery(getNameQueryToFindUser());
+            List lst = q.list();
+            return lst;
+        } catch (HibernateException e) {
+            throw new Exception(e.getCause().getMessage());
+        } finally {
+            releaseSession(session);
+        }
+
+    }
+
+   
 }
